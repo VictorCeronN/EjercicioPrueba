fc1fc40a769e50d0ee9350c953a9d7c91e8397fb
VictorCeronN <46898122+VictorCeronN@users.noreply.github.com>
2019-01-21 09:41:39 -0600

    Victor Manuel Cerón Navarrete, Grupo 02.

---
+++ Velocidad Promedio.py
@@ -0,0 +1,9 @@
+# Autor: Victor Cerón
+# Calcula la velcidad proedio en un viaje
+
+tiempo = int(input("Teclea el tiempo de viaje en horas enteras "))
+distancia = int(input("Teclea la distancia del viaje en km enteros "))
+
+Velocidad = distancia / tiempo
+
+print("La velocidad promedio es:" , Velocidad , "km/h")
